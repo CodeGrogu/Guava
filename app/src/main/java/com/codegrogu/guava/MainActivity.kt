836abd81fd6b68cfbce31c5fdf40c6f5f7a224b7
Jaden <acehood3556@gmail.com>
2026-04-23 08:01:19 +0200

    Connects main activity to global application state

--- app/src/main/java/com/codegrogu/guava/MainActivity.kt
+++ app/src/main/java/com/codegrogu/guava/MainActivity.kt
@@ -4,24 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.codegrogu.guava.ui.theme.GuavaTheme
+import com.codegrogu.guava.viewmodel.AppViewModel
 
 class MainActivity : ComponentActivity() {
+    private val appViewModel: AppViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             GuavaTheme {
+                val uiState by appViewModel.uiState.collectAsState()
+                
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = uiState.currentUser?.name ?: "Guest",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
