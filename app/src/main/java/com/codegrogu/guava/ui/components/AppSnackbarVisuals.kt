0c8c62c7f6eb9ee57a79ae0eff4a8ac5a6fb0966
Jaden <acehood3556@gmail.com>
2026-05-11 21:58:10 +0200

    Create custom snackbar component for styled notifications

---
+++ app/src/main/java/com/codegrogu/guava/ui/components/AppSnackbarVisuals.kt
@@ -0,0 +1,12 @@
+package com.codegrogu.guava.ui.components
+
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarVisuals
+
+data class AppSnackbarVisuals(
+    override val message: String,
+    override val actionLabel: String? = null,
+    override val duration: SnackbarDuration = SnackbarDuration.Short,
+    override val withDismissAction: Boolean = false,
+    val isError: Boolean = true
+) : SnackbarVisuals
